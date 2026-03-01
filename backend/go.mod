8606469a3ff4bf9adbdacecf3b57b054ae41fdc4
ishraaq2001parvez <i4ishraaq@gmail.com>
2026-03-01 15:11:24 +0530

    added server file

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,8 @@
 module simple_web_project
 
-go 1.24.2
+go 1.25.0
+
+require (
+	golang.org/x/net v0.51.0 // indirect
+	golang.org/x/text v0.34.0 // indirect
+)
--- backend/server.go
+++ backend/server.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
+	const port int = 9090
 	fmt.Println("hello")
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
